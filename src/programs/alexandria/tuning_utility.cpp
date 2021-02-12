feb1db8aea9b99712428a9c2caf41806739c600e
David van der Spoel <davidvanderspoel@gmail.com>
2021-02-12 08:32:34 +0100

    Fixed printing of int64_t

--- src/programs/alexandria/tuning_utility.cpp
+++ src/programs/alexandria/tuning_utility.cpp
@@ -653,7 +653,7 @@ void print_header(FILE                       *fp,
             value = gmx::formatString("%d", *p.u.i);
             break;
         case etINT64:
-            value = gmx::formatString("%lld", *p.u.is);
+            value = gmx::formatString("%" PRId64, *p.u.is);
             break;
         case etREAL:
             value = gmx::formatString("%g", *p.u.r);
