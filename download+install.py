ea3608de79b5d836cf903229a57548d3f0262f95
David van der Spoel <spoel@xray.bmc.uu.se>
2021-10-27 08:31:38 +0200

    small updates to install script

--- download+install.py
+++ download+install.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-import os, shutil, argparse, subprocess
+import os, shutil, argparse, subprocess, sys
 
 def parseArguments():
     parser = argparse.ArgumentParser()
     branch = "main"  
     parser.add_argument("-b", "--branch", help="Branch to clone from either ACT or GROMACS depending on branch name. Default is ACT with branch "+branch,             type=str, default=branch)
     parser.add_argument("-f", "--flags",  help="Additional compilation flags",type=str, default="")
-    parser.add_argument("-u", "--user",   help="Account name at gerrit.gromacs.org", type=str, default="dspoel")
+    parser.add_argument("-u", "--user",   help="Account name at gerrit.gromacs.org", type=str, default=None)
     parser.add_argument("-clone", "--clone", help="Clone git repository",     action="store_true")
     parser.add_argument("-ncores","--ncores", help="Number of cores for compiling", type=int, default=8)
-    parser.add_argument("-bt", "--build", help="Build type for cmake. Typicall Release or Debug", type=str, default="Release")
-    parser.add_argument("-sngl", "--single", help="Single precision build (will not work well)", action="store_true")
+    parser.add_argument("-bt", "--build", help="Build type for cmake. Typically Release or Debug, but Profile and ASAN (Adress Sanitizer) are available as well.", type=str, default="Release")
+    parser.add_argument("-single", "--single", help="Single precision build (will not work well)", action="store_true")
     parser.add_argument("-cln", "--cln", help="Use the Class Library for Numbers", action="store_true")
   
     args = parser.parse_args()
@@ -22,13 +22,19 @@ if __name__ == '__main__':
     act  = args.branch == "main" or args.branch == "david"
     if act:
         if args.clone:
-            os.system("git clone git@github.com:dspoel/ACT.git")
+            if args.user:
+                os.system("git clone git@github.com:dspoel/ACT.git")
+            else:
+                os.system("git clone https://github.com/dspoel/ACT.git")
         swdir = "ACT"
     else:
         os.makedirs(args.branch, exist_ok=True)
         os.chdir(args.branch)
         if args.clone:
-            os.system("git clone git@gitlab.com:gromacs/gromacs.git")
+            if args.user:
+                os.system("git clone git@gitlab.com:gromacs/gromacs.git")
+            else:
+                os.system("git clone https://gitlab.com:/gromacs/gromacs.git")
         swdir = "gromacs"
     if not os.path.isdir(swdir):
         print("No directory %s. Maybe you want to use the -clone flag" % swdir)
@@ -44,19 +50,20 @@ if __name__ == '__main__':
     elif "SNIC_SITE" in os.environ:
         HOST = os.environ["SNIC_SITE"]
     else:
-        result = subprocess.run(['hostname'], stdout=subprocess.PIPE)
-        HOST = result.stdout.decode('utf-8').strip().split(".")[0]
+        HOST = sys.platform
 
     extra_dirs = []
     HOMEDIR    = os.environ["HOME"]
     DEST       = ( "%s/%s-%s/" % ( HOMEDIR, swdir, args.branch ) )
     LBFLAGS    = ""
-    if HOST in [ "BirdMac", "blackbird" ]:
+    mpirun     = shutil.which("srun")
+    if HOST == "darwin":
         # MacOS machines, feel free to add your machine to the list
         anaconda = ("%s/opt/miniconda3/lib" % HOMEDIR)
         LAPACK   = ( "%s/liblapack.dylib" % anaconda)
         BLAS     = ( "%s/libblas.dylib" % anaconda)
         LBFLAGS  = ( "-DGMX_BLAS_USER=%s -DGMX_LAPACK_USER=%s" % ( BLAS, LAPACK ) )
+        mpirun   = shutil.which("mpirun")
     elif HOST.find("tetralith") >= 0:
         HOMEDIR   = HOMEDIR + "/wd"
         LAPACK = "/software/sse/easybuild/prefix/software/ScaLAPACK/2.0.2-gompi-2018a-OpenBLAS-0.2.20/lib/libscalapack.a" 
@@ -80,7 +87,7 @@ if __name__ == '__main__':
         sys.exit("Cannot find the MPI enabled mpicc and mpicxx compilers")
 
     gmxdouble = ""
-    FLAGS = ("-DMPIEXEC=/usr/bin/srun -DMPIEXEC_NUMPROC_FLAG='-n' -DGMX_X11=OFF -DGMX_LOAD_PLUGINS=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_OPENMP=OFF -DGMX_MPI=ON -DGMX_GPU=OFF -DCMAKE_INSTALL_PREFIX=%s -DCMAKE_CXX_COMPILER=%s -DCMAKE_C_COMPILER=%s -DCMAKE_BUILD_TYPE=%s -DCMAKE_PREFIX_PATH='%s' -DGMX_BUILD_MANUAL=OFF -DGMX_COMPACT_DOXYGEN=ON -DREGRESSIONTEST_DOWNLOAD=OFF -DGMX_DEFAULT_SUFFIX=OFF -DGMX_LIBXML2=ON -DGMX_EXTERNAL_BLAS=ON -DGMX_EXTERNAL_LAPACK=ON %s" % ( DEST, CXX, CC, args.build, PPATH, LBFLAGS ) )
+    FLAGS = ("-DMPIEXEC=%s -DMPIEXEC_NUMPROC_FLAG='-n' -DGMX_X11=OFF -DGMX_LOAD_PLUGINS=OFF -DBUILD_SHARED_LIBS=OFF -DGMX_OPENMP=OFF -DGMX_MPI=ON -DGMX_GPU=OFF -DCMAKE_INSTALL_PREFIX=%s -DCMAKE_CXX_COMPILER=%s -DCMAKE_C_COMPILER=%s -DCMAKE_BUILD_TYPE=%s -DCMAKE_PREFIX_PATH='%s' -DGMX_BUILD_MANUAL=OFF -DGMX_COMPACT_DOXYGEN=ON -DREGRESSIONTEST_DOWNLOAD=OFF -DGMX_DEFAULT_SUFFIX=OFF -DGMX_LIBXML2=ON -DGMX_EXTERNAL_BLAS=ON -DGMX_EXTERNAL_LAPACK=ON %s" % ( mpirun, DEST, CXX, CC, args.build, PPATH, LBFLAGS ) )
     if args.cln:
         FLAGS += " -DGMX_CLN=ON"
     if not args.single:
