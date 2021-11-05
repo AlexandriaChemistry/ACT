3e914dfa94a264228d7ee4dcb4cc761fbe5ed941
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-05 20:12:37 +0100

    Adjustments + Roulette + SinglePoint

--- src/ga/selection.cpp
+++ src/ga/selection.cpp
@@ -1,3 +1,6 @@
+#include <time.h>
+#include <tuple>
+
 #include "selection.h"
 
 /*!
@@ -8,6 +11,18 @@
  * @param popSize the number of individuals in the population.
  * @return a pointer to the selected individual.
  */
-double* rouletteWheel(double** population, double* normalizedFitness, const int popSize) {
-    return null;
+double* rouletteWheel(double population[][], double normalizedFitness[], const int popSize) {
+
+//    srand((unsigned) time(NULL));
+
+    double randNumb = (double) rand()/RAND_MAX;
+    int i = 0;
+
+    while (randNumb > 0) {
+        randNumb -= normalizedFitness[i];
+        i += 1;
+    }
+
+    return population[i-1];
+
 }
