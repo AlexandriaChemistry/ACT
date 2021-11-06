e0320862a773210285870b7646c043e5b7ad4f1a
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-06 08:30:35 +0100

    Removed some stuff, created GeneticAlgorithm class

--- src/ga/crossover.cpp
+++ src/ga/crossover.cpp
@@ -1,16 +1,1 @@
-#include <tuple>
-
 #include "crossover.h"
-
-void singlePoint(double parent1[], double parent2[], double child1[], double child2[], const int length) {
-    const int index = rand() % length;
-    int i;
-    for (i = 0; i < index; i++) {
-        child1[i] = parent1[i];
-        child2[i] = parent2[i];
-    }
-    for (i = index; i < length; i++) {
-        child1[i] = parent2[i];
-        child2[i] = parent1[i];
-    }
-}
