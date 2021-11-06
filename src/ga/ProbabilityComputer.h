acd81137415aefede46c8f0a5fbb3db5b573d112
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-06 17:01:04 +0100

    Two classes to go

---
+++ src/ga/ProbabilityComputer.h
@@ -0,0 +1,20 @@
+#ifndef ACT_PROBABILITYCOMPUTER_H
+#define ACT_PROBABILITYCOMPUTER_H
+
+/*!
+ * Abstract class for computing the selection probability of each individual in the population
+ */
+class ProbabilityComputer {
+
+public:
+    /*!
+     * Compute the probability of each individual in the population
+     * @param fitness   the fitness of each individual
+     * @param prob      structure to store the probabilities
+     * @param popSize   size of the population
+     */
+    virtual void compute(double* const fitness, double* const prob, const int popSize);
+
+};
+
+#endif //ACT_PROBABILITYCOMPUTER_H
