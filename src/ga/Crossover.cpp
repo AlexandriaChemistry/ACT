65eaf6c66b1c6a8580656c0e40e2d3960e49883c
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-08 21:10:12 +0100

    Added single-point and double-point crossovers

--- src/ga/Crossover.cpp
+++ src/ga/Crossover.cpp
@@ -1,1 +1,59 @@
 #include "Crossover.h"
+
+#include <random>
+#include <cmath>
+#include <algorithm>
+
+
+Crossover::Crossover(const int chromosomeLength) {
+    gen = std::mt19937(rd);
+    // Exclude first and last indices. Otherwise, there is no crossover.
+    dis = std::uniform_int_distribution<>(1, chromosomeLength - 2);
+}
+
+
+void SinglePointCrossover::offspring(const vector parent1, const vector parent2, const vector child1,
+                                     const vector child2, const int length) {
+
+    const int index = dis(gen);
+
+    int i;
+    for (i = 0; i < index; i++) {
+        child1[i] = parent1[i];
+        child2[i] = parent2[i];
+    }
+    for (i = index; i < length; i++) {
+        child1[i] = parent2[i];
+        child2[i] = parent1[i];
+    }
+
+}
+
+
+void DoublePointCrossover::offspring(const vector parent1, const vector parent2, const vector child1,
+                                     const vector child2, const int length) {
+
+    const int tmp1 = dis(gen);
+    int tmp2 = dis(gen);
+    while (tmp1 == tmp2) {
+        tmp2 = dis(gen);
+    }
+    const int diff = std::abs(tmp1 - tmp2);
+
+    const int index1 = std::min(tmp1, tmp2);
+
+    int i;
+    for (i = 0; i < index1; i++) {
+        child1[i] = parent1[i];
+        child2[i] = parent2[i];
+    }
+    for (i = index1; i < index1 + diff; i++) {
+        child1[i] = parent2[i];
+        child2[i] = parent1[i];
+    }
+    for (i = index1 + diff; i < length; i++) {
+        child1[i] = parent1[i];
+        child2[i] = parent2[i];
+    }
+
+}
--- src/ga/Crossover.h
+++ src/ga/Crossover.h
@@ -1,22 +1,64 @@
 #ifndef ACT_CROSSOVER_H
 #define ACT_CROSSOVER_H
 
+#include <random>
+
 #include "aliases.h"
+
 
 /*!
  * Abstract class to perform crossover
  */
 class Crossover {
 
+protected:
+    std::random_device rd;  // Will be used to obtain a seed for the random number engine
+    std::mt19937 gen; // Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> dis;
+
 public:
+    /*!
+     * Create a new crossover object.
+     * @param chromosomeLength  length of the chromosome
+     */
+    Crossover(const int chromosomeLength);
+
     /*!
      * Perform crossover operation
      * @param parent1   the first parent
      * @param parent2   the second parent
      * @param child1    the first child to write to
      * @param child2    the second child to write to
+     * @param length    length of each individual
      */
-    virtual void offspring(const vector parent1, const vector parent2, const vector child1, const vector child2);
+    virtual void offspring(const vector parent1, const vector parent2, const vector child1, const vector child2,
+                           const int length);
+
+};
+
+
+/*!
+ * Class for single-point crossover operation.
+ */
+class SinglePointCrossover : public Crossover {
+
+public:
+    SinglePointCrossover(const int chromosomeLength): Crossover(chromosomeLength);
+    void offspring(const vector parent1, const vector parent2, const vector child1, const vector child2,
+                   const int length);
+
+};
+
+
+/*!
+ * Class for double-point crossover operation.
+ */
+class DoublePointCrossover : public Crossover {
+
+public:
+    DoublePointCrossover(const int chromosomeLength): Crossover(chromosomeLength);
+    void offspring(const vector parent1, const vector parent2, const vector child1, const vector child2,
+                   const int length);
 
 };
 
