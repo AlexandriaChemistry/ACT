e650eae73e78d9115e39a35b75964f36a1add317
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-08 11:08:05 +0100

    Created aliases

---
+++ src/ga/aliases.h
@@ -0,0 +1,28 @@
+#ifndef ACT_ALIASES_H
+#define ACT_ALIASES_H
+
+#include <tuple>
+#include <vector>
+
+/*!
+ * Regular 1D array of double precision floating point numbers
+ */
+using vector = std::vector<double>;
+
+/*!
+ * 2D matrix of double precision floating point numbers
+ */
+using matrix = std::vector<vector>;
+
+/*!
+ * Result structure of genetic algorithm
+ */
+typedef struct ga_result {
+    matrix pop;
+    vector fitness;
+    vector bestIndividual;
+    double bestFitness;
+    int generations;
+} ga_result_t;
+
+#endif //ACT_ALIASES_H
