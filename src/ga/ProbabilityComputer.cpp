3607b15f29a3678810220d3518cf16be4fa4736a
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-08 21:44:36 +0100

    Added FitnessProbabilityComputer

--- src/ga/ProbabilityComputer.cpp
+++ src/ga/ProbabilityComputer.cpp
@@ -1,1 +1,13 @@
 #include "ProbabilityComputer.h"
+
+
+void FitnessProbabilityComputer::compute(const vector fitness, const vector prob, const int popSize) {
+    double total = 0;
+    int i;
+    for (i = 0; i < popSize; i++) {
+        total += fitness[i];
+    }
+    for (i = 0; i < popSize; i++) {
+        prob[i] = fitness[i] / total;
+    }
+}
