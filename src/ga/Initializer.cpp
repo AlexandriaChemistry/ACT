977f7a5310a5fbf497c1b17e33fcfb67491377b0
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-10 13:03:50 +0100

    Oskar taking over

--- src/ga/Initializer.cpp
+++ src/ga/Initializer.cpp
@@ -1,1 +1,13 @@
 #include "Initializer.h"
+
+SimpleInitializer::SimpleInitializer(const double min, const double max) {
+    gen = std::mt19937(rd);
+    dis = std::uniform_real_distribution<>(min, max);
+}
+
+void SimpleInitializer::initialize(const vector     individual,
+                                   const int        length) {
+    for (int i = 0; i < length; i++) {
+        individual[i] = dis(gen);
+    }
+}
