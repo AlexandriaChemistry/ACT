938f00570fd4e48f99b4e2723b034ea414472b82
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-10 11:39:51 +0100

    SimpleTerminator

--- src/ga/helpers.h
+++ src/ga/helpers.h
@@ -3,8 +3,22 @@
 
 #include "aliases.h"
 
-matrix allocateMatrix(const int n, const int m);
-void copyVectorValues(const vector arr1, const vector arr2, const int left, const int right);
-void copyMatrixValues(const matrix mat1, const matrix mat2, const int i1, const int i2, const int j1, const int j2);
+matrix allocateMatrix(const int     n,
+                      const int     m);
+
+void copyVectorValues(const vector  arr1,
+                      const vector  arr2,
+                      const int     left,
+                      const int     right);
+
+void copyMatrixValues(const matrix  mat1,
+                      const matrix  mat2,
+                      const int     i1,
+                      const int     i2,
+                      const int     j1,
+                      const int     j2);
+
+int findMaximumIndex(const vector   vec,
+                     const int      len);
 
 #endif //ACT_HELPERS_H
