9ce00ded0c39e712e4826c4a4e06bba3aec21d33
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-10 13:20:47 +0100

    Typos and PercentMutator

--- src/ga/Mutator.h
+++ src/ga/Mutator.h
@@ -1,6 +1,8 @@
 #ifndef ACT_MUTATOR_H
 #define ACT_MUTATOR_H
 
+#include <random>
+
 /*!
  * Abstract class for gene mutation
  */
@@ -12,6 +14,26 @@ public:
      * @param gene  pointer to the gene
      */
     virtual void mutate(double* const gene);
+
+};
+
+/*!
+ * Class for percentual gene mutation
+ * Modifies a gene by a maximum of <frac*100>% of its value
+ */
+class PercentMutator {
+
+    std::random_device rd;  // Will be used to obtain a seed for the random number engine
+    std::mt19937 gen; // Standard mersenne_twister_engine seeded with rd()
+    std::uniform_real_distribution<> dis;
+
+public:
+    /*!
+     * Create a new PercentMutation object
+     * @param frac  the fraction of change in [0, 1]
+     */
+    PercentMutator(const double frac);
+    void mutate(double* const gene);
 
 };
 
