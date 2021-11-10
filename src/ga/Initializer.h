561b4c9d350d3d860acab7e61b7edaf786b56a47
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-10 20:13:39 +0100

    Quite some debugging to do tomorrow

--- src/ga/Initializer.h
+++ src/ga/Initializer.h
@@ -25,7 +25,7 @@ class SimpleInitializer : public Initializer {
     // Random number generation
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937 gen; // Standard mersenne_twister_engine seeded with rd()
-    std::uniform_real_distribution<> dis;
+    std::uniform_real_distribution<double> dis;
 
 public:
     /*!
