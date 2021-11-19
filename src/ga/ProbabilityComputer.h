717d43a03126023f21625ea4ceaed7e7240182a2
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-11-19 15:41:58 +0100

    Update ProbabilityComputer.h
    Doxygen (Latex to be tested)

--- src/ga/ProbabilityComputer.h
+++ src/ga/ProbabilityComputer.h
@@ -17,8 +17,8 @@ namespace ga
         /*!
          * Compute the probability of each individual in the population
          * @param fitness   the fitness of each individual
-         * @param prob      structure to store the probabilities
-         * @param popSize   size of the population
+         * @param prob      structure to store the probability for each individual
+         * @param popSize   number of individuals in the population
          */
         virtual void compute(const vector&  fitness,
                                    vector&  prob,
@@ -28,7 +28,12 @@ namespace ga
 
 
     /*!
-     * Class for fitness-based probability computation
+     * Class for fitness-based probability computation, that is, the probability is proportional to the fitness.<br>
+     * The probability of individual \f$i\f$ can be written as
+     * \f[
+     *      p_i = \frac { f_i } { \sum \limits_{ j=1 }^{ n } f_j },
+     * \f]
+     * where \f$n\f$ is the number of individuals in the population.
      */
     class FitnessProbabilityComputer : public ProbabilityComputer
     {
@@ -42,12 +47,20 @@ namespace ga
 
 
     /*!
-     * Class for Boltzmann temperature probability computation
+     * Class for Boltzmann temperature probability computation.<br>
+     * The probability of individual \f$i\f$ can be written as
+     * \f[
+     *      p_i = \frac{ e^{ f_i / T } }{ \sum \limits_{ j=1 }^{ n } e^{ f_j / T } },
+     * \f]
+     * where \f$n\f$ is the number of individuals in the population, and \f$T \in \mathbb{R}\f$ is the Boltzmann
+     * temperature parameter.
      */
     class BoltzmannProbabilityComputer : public ProbabilityComputer
     {
 
+        //! The temperature parameter
         double temperature;
+        //! Stores e^fitness for each individual
         vector exponentials;
 
     public:
@@ -67,11 +80,17 @@ namespace ga
 
 
     /*!
-     * Class for Rank probability computation
+     * Class for Rank probability computation, that is, the probability of an individual is proportional to its rank.
+     * Rank 1 means highest fitness, rank 2 second highest, etc.<br>
+     * If there are \f$R\f$ ranks, then the probability of the individual at rank \f$i\f$ is
+     * \f[
+     *      p_i = \frac{R - i + 1}{ R \left( R+1 \right) / 2 }
+     * \f]
      */
     class RankProbabilityComputer : public ProbabilityComputer
     {
 
+        //! Stores the sum of ranks. E.g., if there are 10 individuals, the sum of ranks is 1 + 2  + 3 + ... + 9 + 10
         double sumOfRanks;
 
     public:
@@ -82,7 +101,7 @@ namespace ga
         RankProbabilityComputer(const int popSize);
 
         /*!
-         * Here we assume that population and fitness are sorted in a descending manner
+         * Here we assume that population and fitness are sorted in a descending manner.
          */
         void compute(const vector&  fitness,
                            vector&  prob,
