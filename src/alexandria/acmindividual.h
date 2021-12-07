cafc87f853c793f47879b592bd58648509a89429
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-12-07 21:49:59 +0100

    Simpler deep-copy

--- src/alexandria/acmindividual.h
+++ src/alexandria/acmindividual.h
@@ -77,20 +77,22 @@ public:
         bestParam_.resize(nParams, 0.0);
 
         // Deep-copy targets_ from sii_
-        for (auto const& imsMapPair : sii_->targets())
-        {
-            const iMolSelect ims = imsMapPair.first;
-            std::map<eRMS, FittingTarget> eRMSTargetMap;
-            for (auto const& eRMSTargetPair : imsMapPair.second)
-            {
-                const eRMS erms = eRMSTargetPair.first;
-                const FittingTarget tmpTarget = eRMSTargetPair.second;
-                eRMSTargetMap.insert(std::pair<eRMS, FittingTarget>(erms, tmpTarget));
-            }
-            targets_.insert(std::pair<eRMS, std::map<eRMS, FittingTarget>>(ims, eRMSTargetMap));
-        }
+        // for (auto const& imsMapPair : sii_->targets())
+        // {
+        //     const iMolSelect ims = imsMapPair.first;
+        //     std::map<eRMS, FittingTarget> eRMSTargetMap;
+        //     for (auto const& eRMSTargetPair : imsMapPair.second)
+        //     {
+        //         const eRMS erms = eRMSTargetPair.first;
+        //         const FittingTarget tmpTarget = eRMSTargetPair.second;
+        //         eRMSTargetMap.insert(std::pair<eRMS, FittingTarget>(erms, tmpTarget));
+        //     }
+        //     targets_.insert(std::pair<eRMS, std::map<eRMS, FittingTarget>>(ims, eRMSTargetMap));
+        // }
+        targets_ = sii->targets();  // This should make a deep copy if the copy constructors are well made
 
         // TODO: copy poldata from sii_
+        
     }
 
     /* * * * * * * * * * * * * * * * * * * * * *
