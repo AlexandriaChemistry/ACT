ef6e4611e6f8173171d2aa41e9c75e5917f771bb
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2021-12-25 12:40:00 +0100

    Removed useless code and adjusted Terminator

--- src/ga/Initializer.h
+++ src/ga/Initializer.h
@@ -35,37 +35,6 @@ public:
 };
 
 
-/*!
- * Toy initializer. Initializes values randomly in range [\p min, \p max]
- */
-// class SimpleInitializer : public Initializer
-// {
-
-// private:
-
-//     std::random_device                      rd;
-//     std::mt19937                            gen;
-//     std::uniform_real_distribution<double>  dis;
-
-// public:
-
-//     /*!
-//      * Create a new SimpleInitializer object
-//      * @param min   minimum value to give to a gene
-//      * @param max   maximum value to give to a gene
-//      */
-//     SimpleInitializer(const double min,
-//                       const double max)
-//     : gen(rd()), dis(std::uniform_real_distribution<>(min, max))
-//     {
-//         gen.seed(::time(NULL));
-//     }
-
-//     virtual void initialize(Individual  *individual);
-
-// };
-
-
 } //namespace ga
 
 
--- src/ga/Terminator.cpp
+++ src/ga/Terminator.cpp
@@ -1,42 +1,18 @@
 #include "Terminator.h"
 
-#include "ga_helpers.h"
-#include "aliases.h"
-
 
 namespace ga
 {
 
 
 /* * * * * * * * * * * * * * * * * * * * * *
-* BEGIN: SimpleTerminator                  *
-* * * * * * * * * * * * * * * * * * * * * */
-
-bool SimpleTerminator::terminate(const matrix  &population,
-                                 const vector  &fitness,
-                                 const int      generationNumber,
-                                 const int      popSize,
-                                 const int      chromosomeLength)
-{
-    double maximumFitness = fitness[findMaximumIndex(fitness, popSize)];
-    return maximumFitness >= 1 / (tolerance * chromosomeLength);
-}
-
-/* * * * * * * * * * * * * * * * * * * * * *
-* END: SimpleTerminator                    *
-* * * * * * * * * * * * * * * * * * * * * */
-
-/* * * * * * * * * * * * * * * * * * * * * *
 * BEGIN: GenerationTerminator              *
 * * * * * * * * * * * * * * * * * * * * * */
 
-bool GenerationTerminator::terminate(const matrix  &population,
-                                     const vector  &fitness,
-                                     const int      generationNumber,
-                                     const int      popSize,
-                                     const int      chromosomeLength)
+bool GenerationTerminator::terminate(const std::vector<Individual*>  &pop,
+                                     const int                        generationNumber)
 {
-    return generationNumber >= maxGenerations;
+    return generationNumber >= maxGenerations_;
 }
 
 /* * * * * * * * * * * * * * * * * * * * * *
