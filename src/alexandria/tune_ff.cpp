de0ef2d611d2a759a12f1e8e62dadbfc59db354d
Julián Marrades <julianramon.marradesfurquet.8049@student.uu.se>
2022-04-27 20:05:07 +0200

    Refined the CalcDev estimate printing a bit
    It printed the number of generations when MCMC was selected, which makes no sense...

--- src/alexandria/tune_ff.cpp
+++ src/alexandria/tune_ff.cpp
@@ -439,11 +439,22 @@ void OptACM::printNumCalcDevEstimate()
     nCalcDevTest += 1;  // At the end, for the best
     nCalcDevIgnore += 1;  // At the end, for the best
 
-    fprintf(
-        logFile(),
-        "\nMaximum number of fitness computations to be done for %d generations:\n  - Train: %ld\n  - Test: %ld\n  - Ignore: %ld\nConsider that in GA and HYBRID each penalty conveys an extra fitness computation per genome for the training set.\n\n",
-        gach_.maxGenerations(), nCalcDevTrain, nCalcDevTest, nCalcDevIgnore
-    );
+    if (gach_.optimizer() == OptimizerAlg::MCMC)
+    {
+        fprintf(
+            logFile(),
+            "\nMaximum number of fitness computations to be done for MCMC:\n  - Train: %ld\n  - Test: %ld\n  - Ignore: %ld\n\n",
+            nCalcDevTrain, nCalcDevTest, nCalcDevIgnore
+        );
+    }
+    else  // GA/HYBRID
+    {
+        fprintf(
+            logFile(),
+            "\nMaximum number of fitness computations to be done for %d generations:\n  - Train: %ld\n  - Test: %ld\n  - Ignore: %ld\nConsider that in GA and HYBRID each penalty conveys an extra fitness computation per genome for the training set.\n\n",
+            gach_.maxGenerations(), nCalcDevTrain, nCalcDevTest, nCalcDevIgnore
+        );
+    }
 }
 
 void OptACM::printGenomeTable(const ga::Genome   &genome,
