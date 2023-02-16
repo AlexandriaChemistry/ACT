ee0d57312ab46490cf7268afb2ac082e5d720a76
Julián Marrades <julian@marrad.es>
2023-02-16 18:00:54 +0100

    Started with conda packaging

---
+++ conda/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Shell script for macOS and Linux
+# Can use variables such as ARCH (architecture) and PREFIX (build environment prefix)
+
+# Install custom OpenBabel
+
+# Install ACT
