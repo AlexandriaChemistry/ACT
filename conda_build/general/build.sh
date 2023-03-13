46b729992ffa6a331b25310398b929e1b0f8a86a
Julián Marrades <julian@marrad.es>
2023-03-13 18:19:05 +0100

    OpenBabel installs with errors

--- conda_build/general/build.sh
+++ conda_build/general/build.sh
@@ -3,10 +3,9 @@
 # Shell script for macOS and Linux
 # Can use variables such as ARCH (architecture) and PREFIX (future environment prefix)
 
-ls
-
 # Installing openbabel
 echo "Installing openbabel..."
+
 cd ${SRC_DIR}/openbabel
 git checkout alexandria
 mkdir ${SRC_DIR}/openbabel/build
@@ -14,7 +13,7 @@ cd ${SRC_DIR}/openbabel/build
 
 [ ${ARCH} == "64" ] && DLIB_SUFFIX="-DLIB_SUFFIX=64" || DLIB_SUFFIX=""
 # echo ${DLIB_SUFFIX}
-cmake \
+cmake -S${SRC_DIR}/openbabel -B${SRC_DIR}/openbabel/build \
 	-DBUILD_SHARED=ON \
 	-DBUILD_GUI=OFF \
 	-DCMAKE_PREFIX_PATH=${BUILD_PREFIX} \
@@ -25,12 +24,11 @@ cmake \
 	-DWITH_MAEPARSER=OFF \
 	-DCMAKE_CXX_COMPILER=${CXX_FOR_BUILD} \
 	-DCMAKE_C_COMPILER=${CC_FOR_BUILD} \
-	-DCMAKE_C_FLAGS=${CFLAGS} \
-	-DCMAKE_CXX_FLAGS=${CXXFLAGS} \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
 	${DLIB_SUFFIX} \
 	..
-make -j ${CPU_COUNT} install
+# -DCMAKE_C_FLAGS=${CFLAGS} \
+# -DCMAKE_CXX_FLAGS=${CXXFLAGS} \
 
-ls
+(cd ${SRC_DIR}/openbabel/build && make -j ${CPU_COUNT} install)
