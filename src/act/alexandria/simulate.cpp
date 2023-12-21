8abb4b960e6d39371a20439a3f633a833352628b
David van der Spoel <david.vanderspoel@gmail.com>
2023-12-21 14:36:22 +0100

    Added code to print interaction energy when a dimer is minimized.

--- src/act/alexandria/simulate.cpp
+++ src/act/alexandria/simulate.cpp
@@ -290,8 +290,20 @@ int simulate(int argc, char *argv[])
                 if (eMinimizeStatus::OK == eMin)
                 {
                     auto rmsd = molhandler.coordinateRmsd(&actmol, coords, &xmin);
-                    fprintf(logFile, "Final energy: %g. RMSD wrt original structure %g nm.\n",
+                    fprintf(logFile, "Final energy: %g RMSD wrt original structure %g nm.\n",
                             energies[InteractionType::EPOT], rmsd);
+                    if (actmol.fragmentHandler()->topologies().size() == 2)
+                    {
+                        std::map<InteractionType, double> einter;
+                        std::vector<gmx::RVec>            interactionForces;
+                        actmol.calculateInteractionEnergy(&pd, forceComp, &einter,
+                                                          &interactionForces, &coords);
+                        for(const auto &ei : einter)
+                        {
+                            fprintf(logFile, "Interaction energy %s: %g\n",
+                                    interactionTypeToString(ei.first).c_str(), ei.second);
+                        }
+                    }
                     JsonTree jtener("Energies");
                     std::string unit("kJ/mol");
                     for (const auto &ener : energies)
