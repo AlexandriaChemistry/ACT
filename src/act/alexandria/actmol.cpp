c015c8b2203473589b52cd2c815b6488290eaa98
David van der Spoel <david.vanderspoel@gmail.com>
2024-04-01 09:51:57 +0200

    Removed annoying warning.
    Part of #279

--- src/act/alexandria/actmol.cpp
+++ src/act/alexandria/actmol.cpp
@@ -250,7 +250,7 @@ static std::vector<gmx::RVec> experCoords(const std::vector<gmx::RVec> &xxx,
     fcomp.generateVsites(topology, &coords);
     return coords;
 }
-
+static bool warningSPdone = false;
 std::vector<gmx::RVec> ACTMol::xOriginal() const
 {
     auto exper = findExperimentConst(JobType::OPT);
@@ -265,11 +265,12 @@ std::vector<gmx::RVec> ACTMol::xOriginal() const
         {
             GMX_THROW(gmx::InternalError(gmx::formatString("No structure at all for %s", getMolname().c_str()).c_str()));
         }
-        else
+        else if (!warningSPdone)
         {
             fprintf(stderr, "Warning: No calculation for %s with jobtype %s or %s, using first %s calc.\n",
                     getMolname().c_str(), jobType2string(JobType::OPT), jobType2string(JobType::TOPOLOGY),
                     jobType2string(JobType::SP));
+            warningSPdone = true;
         }
     }
 
