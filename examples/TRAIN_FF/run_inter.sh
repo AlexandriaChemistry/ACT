d9cabd2dd757760d859bd0643481f3cafd95b28b
David van der Spoel <david.vanderspoel@gmail.com>
2025-03-12 14:34:25 +0100

    Updated script in examples

--- examples/TRAIN_FF/run_inter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#SBATCH -n 16
-#SBATCH -t 24:00:00
-
-# Remove oversubscribe option if your mpirun does not support it
-mpirun -n 16 -oversubscribe alexandria train_ff -ff ../myff2.xml -mp ../XML/alcohol-sapt.xml -sel ../SELECTIONS/alcohol-dimer.dat -optimizer HYBRID -max_generations 10 -max_iter 20 -temp 1 -anneal_globally -anneal 0  -printSP -pop_size 16 -fit 'chi eta zeta sigma epsilon' -fc_inter 1 -n_crossovers 2 -cp_gen_interval 5 -cp_pop_frac 0.2 -fc_bound 1000  -fc_charge 20000 -v 4 -o inter.xml -g train_inter.log
-
