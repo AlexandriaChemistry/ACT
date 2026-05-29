27fed7054e183fecf303370adbb81156d0f74b1c
David van der Spoel <david.vanderspoel@gmail.com>
2026-05-29 06:37:16 +0200

    Added missing file

---
+++ src/act/ga/tests/penalizerTest.cpp
@@ -0,0 +1,158 @@
+/*
+ * This source file is part of the Alexandria program.
+ *
+ * Copyright (C) 2026
+ *
+ * Developers:
+ *             Mohammad Mehdi Ghahremanpour,
+ *             Julian Marrades,
+ *             Marie-Madeleine Walz,
+ *             Paul J. van Maaren,
+ *             David van der Spoel (Project leader)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+/*! \internal \brief
+ * Implements test of the penalizer algorithgm
+ * \author David van der Spoel <david.vanderspoel@icm.uu.se>
+ */
+#include <cmath>
+
+#include <map>
+
+#include <gtest/gtest.h>
+
+#include "act/alexandria/acminitializer.h"
+#include "act/ga/gene_pool.h"
+#include "act/ga/penalizer.h"
+#include "ga_test_helper.h"
+
+#include "testutils/cmdlinetest.h"
+#include "testutils/mpitest.h"
+#include "testutils/refdata.h"
+#include "testutils/testasserts.h"
+#include "testutils/testfilemanager.h"
+
+
+namespace ga
+{
+
+namespace
+{
+
+class PenalizerTest : public gmx::test::CommandLineTestBase
+{
+protected:
+    
+ 
+    PenalizerTest ()
+    {
+    }
+
+    // Static initiation, only run once every test.
+    static void SetUpTestCase()
+    {
+    }
+
+    static void TearDownTestCase()
+    {
+    }
+
+    void checkGenePool(gmx::test::TestReferenceChecker *checker_,
+                       const GenePool                  *pool,
+                       const std::string                label)
+    {
+        auto checker = checker_->checkCompound("GenePool", label);
+        int i = 0;
+        for(const auto &gene : pool->genePool())
+        {
+            auto gcheck = checker.checkCompound("Gene", std::to_string(i));
+            gcheck.checkSequence(gene.bases().begin(),
+                                 gene.bases().end(),
+                                 label.c_str());
+            std::string flabel("Fitness_");
+            flabel += std::to_string(i);
+            gcheck.checkFloat(gene.fitness(iMolSelect::Train), flabel.c_str());
+            i += 1;
+        }
+    }
+
+    void vfpTest(bool logVolume, double volFracLimit, double popFrac)
+    {
+        // Checker
+        gmx::test::TestReferenceChecker checker(this->rootChecker());
+        auto tolerance = gmx::test::relativeToleranceAsFloatingPoint(1.0, 1e-4);
+        checker.setDefaultTolerance(tolerance);
+        // Make some genes
+        std::vector<std::vector<double>> genes = {
+            { 3.0, 3.9, 6.8 },
+            { 4.6, 0.3, 12.7 },
+            { 5.8, 14, -3 },
+            { 5.0, -14, 0.0 }
+        };
+        GenePool gp(genes.size());
+        double totalVolume = 8.0 * 15.0 * 25.0 * 30.0;
+        for(size_t i = 0; i < genes.size(); i++)
+        {
+            gp.addGenome(genes[i], i);
+        }
+        checkGenePool(&checker, &gp, "Before");
+        alexandria::GaTestHelper gth(1, {}, {});
+        VolumeFractionPenalizer vfp(nullptr, logVolume, totalVolume,
+                                    volFracLimit, popFrac,
+                                    static_cast<Initializer *>(gth.initializer));
+        bool penalized = vfp.penalize(nullptr, &gp, 1);
+        EXPECT_TRUE(penalized);
+        checkGenePool(&checker, &gp, "After");
+    }
+};
+
+TEST_F (PenalizerTest, Volume1)
+{
+    bool   logVolume    = false;
+    double volFracLimit = 0.2;
+    double popFrac      = 0.25;
+    vfpTest(logVolume, volFracLimit, popFrac);
+}
+
+TEST_F (PenalizerTest, LogVolume1)
+{
+    bool   logVolume    = true;
+    double volFracLimit = 0.2;
+    double popFrac      = 0.25;
+    vfpTest(logVolume, volFracLimit, popFrac);
+}
+
+TEST_F (PenalizerTest, Volume2)
+{
+    bool   logVolume    = false;
+    double volFracLimit = 0.5;
+    double popFrac      = 0.4;
+    vfpTest(logVolume, volFracLimit, popFrac);
+}
+
+TEST_F (PenalizerTest, LogVolume2)
+{
+    bool   logVolume    = true;
+    double volFracLimit = 0.5;
+    double popFrac      = 0.4;
+    vfpTest(logVolume, volFracLimit, popFrac);
+}
+
+}
+
+} 
