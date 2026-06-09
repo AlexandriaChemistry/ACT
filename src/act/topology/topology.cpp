130613fe76155361273a9b56ca015a55115420da
David van der Spoel <david.vanderspoel@gmail.com>
2026-06-09 21:11:29 +0200

    Downgraded confusing warning message to verbose

--- src/act/topology/topology.cpp
+++ src/act/topology/topology.cpp
@@ -1809,8 +1809,8 @@ static void fillParams(MsgHandler                      *msghandler,
     }
     if (found != independent)
     {
-        msg += gmx::formatString(" found %d, expected %d independent parameters", found, independent);
-        msghandler->msg(ACTStatus::Warning, ACTMessage::MissingFFParameter, msg);
+        msg += gmx::formatString(" found %d, expected %d  parameters", found, independent);
+        msghandler->msg(ACTStatus::Verbose, ACTMessage::MissingFFParameter, msg);
     }
 }
 
