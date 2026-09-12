96d00880a399c91169a78d2df9d1f88f7fc96ab2
David van der Spoel <david.vanderspoel@gmail.com>
2026-09-12 15:18:05 +0200

    Made generalized mean even more conservative re: exponent.
    Added 6 new tests.
    
    Part of #1062

--- src/act/forcefield/combinationrules.cpp
+++ src/act/forcefield/combinationrules.cpp
@@ -191,7 +191,7 @@ double combineGeneralizedMean(double x1, double x2, double exponent)
     // double precision is not good enough, however the analytical
     // result converges to the geometric mean. Therefore we approximate
     // the result for small exponents by that.
-    const double tolerance = 1e-6;
+    const double tolerance = 0.1;
     if (std::abs(exponent) < tolerance)
     {
         return std::sqrt(x1*x2);
